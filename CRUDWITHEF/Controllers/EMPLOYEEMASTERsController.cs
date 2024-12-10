88d7771e48e45c1702535ebd8d57d4dd8b474871
admin <admin@DESKTOP-D8PA5JU>
2024-12-10 17:20:17 +0530

    Add project files.

---
+++ CRUDWITHEF/Controllers/EMPLOYEEMASTERsController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using CRUDWITHEF.Models;
+
+namespace CRUDWITHEF.Controllers
+{
+    public class EMPLOYEEMASTERsController : Controller
+    {
+        public CRUDDBEntities db = new CRUDDBEntities();
+
+        public ActionResult Index()
+        {
+            return View(db.EMPLOYEEMASTERs.ToList());
+        }
+
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEEMASTER eMPLOYEEMASTER = db.EMPLOYEEMASTERs.Find(id);
+            if (eMPLOYEEMASTER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEEMASTER);
+        }
+        
+        public ActionResult Create()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ID,EMPCODE,EMPNAME,DESIGNATION,SALARY")] EMPLOYEEMASTER eMPLOYEEMASTER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.EMPLOYEEMASTERs.Add(eMPLOYEEMASTER);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eMPLOYEEMASTER);
+        }
+        
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEEMASTER eMPLOYEEMASTER = db.EMPLOYEEMASTERs.Find(id);
+            if (eMPLOYEEMASTER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEEMASTER);
+        }
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ID,EMPCODE,EMPNAME,DESIGNATION,SALARY")] EMPLOYEEMASTER eMPLOYEEMASTER)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(eMPLOYEEMASTER).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(eMPLOYEEMASTER);
+        }
+        
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            EMPLOYEEMASTER eMPLOYEEMASTER = db.EMPLOYEEMASTERs.Find(id);
+            if (eMPLOYEEMASTER == null)
+            {
+                return HttpNotFound();
+            }
+            return View(eMPLOYEEMASTER);
+        }
+        
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            EMPLOYEEMASTER eMPLOYEEMASTER = db.EMPLOYEEMASTERs.Find(id);
+            db.EMPLOYEEMASTERs.Remove(eMPLOYEEMASTER);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        public ActionResult Search()
+        {
+            return View();
+        }
+        public ActionResult SearchBySalary(decimal salary)
+        {
+            var employees = db.EMPLOYEEMASTERs.Where(e => e.SALARY <= salary).ToList();
+            return Json(new { success = true, employees }, JsonRequestBehavior.AllowGet);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
+
---
+++ CRUDWITHEF/Models/AccountViewModels.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace YourNamespace.Models
+{
+    public class EmployeeViewModel
+    {
+        public List<Employee> Employees { get; set; }
+
+        public class Employee
+        {
+            public string Name { get; set; }
+            public decimal Salary { get; set; }
+        }
+    }
+}
